92e24855752c0c08104102902851e4104eca3858
Joshua Wood <JerichoWall@apu.edu>
2019-02-11 15:23:19 -0800

    Project complete, with several stretch goals

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail';
+
+function Header() {
+    return (
+        <div className="headContainer">
+            <ImageThumbnail />
+            <HeaderContent />
+        </div>    
+    );
+}
+
+export default Header;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Header.css';
+import moment from 'moment';
+
+function HeaderTitle() {
+    return (
+        <div className="titleDisplay">
+                <h4>Lambda School</h4>
+                <p className="stamps">@LambdaSchool - { date }</p>
+        </div>
+    );
+}
+
+export default HeaderTitle;
+
+let date = moment().format("MMM Do YY"); 
